432e2dd19598f9a2a690d906e00441e1b04921d3
Eduardo Schulz <eduardo.schulz@protonmail.com>
2022-11-13 17:59:29 -0300

    add calculos principais

---
+++ src/main/java/org/uniproj/calculadora/Calculadora.java
@@ -0,0 +1,35 @@
+package org.uniproj.calculadora;
+import org.uniproj.pessoa.*;
+public class Calculadora {
+
+    public static void calcularImc(Pessoa pessoa){
+       pessoa.setImc(pessoa.getPeso()/Math.pow(pessoa.getAltura(),2));
+    }
+
+    public static String statusImc(Pessoa pessoa){
+        double imc = pessoa.getImc();
+        if(pessoa.getSexo() == "masculino"){
+            if(imc < 19.1)
+                return "abaixo do peso";
+            else if(imc < 25.8)
+                return "ideal";
+            else
+                return "obeso";
+        }
+        else{
+            if(imc < 20.7)
+                return "abaixo do peso";
+            else if(imc < 26.4)
+                return "ideal";
+            else
+                return "obeso";
+        }
+
+    }
+
+    public static double txGorduraCorporal(Pessoa pessoa){
+        return (1.2 * pessoa.getImc()) - (10.8 * ((pessoa.getSexo() == "feminino")? 0 : 1) + (0.23 * pessoa.getIdade()) - 5.4);
+    }
+
+
+}
---
+++ src/main/java/org/uniproj/pessoa/Pessoa.java
@@ -0,0 +1,74 @@
+package org.uniproj.pessoa;
+
+public class Pessoa {
+    private int idade, peso, altura, imc;
+    private String nome, codigo, sexo;
+
+    public Pessoa(int idade, int peso, int altura, String nome, String codigo) {
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public void setImc(int imc) {
+        this.imc = imc;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getImc(){
+        return imc;
+    }
+
+    public void setImc(double v){
+        this.imc = imc;
+    }
+}
