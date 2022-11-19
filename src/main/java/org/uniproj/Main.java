c89e92928f08c047949b6870212c4e1e6650f932
Eduardo Schulz <eduardo.schulz@protonmail.com>
2022-11-18 21:55:48 -0300

    fix typos and imports

--- src/main/java/org/uniproj/Main.java
+++ src/main/java/org/uniproj/Main.java
@@ -3,15 +3,14 @@ package org.uniproj;
 import org.uniproj.calculadora.Calculadora.*;
 import org.uniproj.pessoa.Pessoa;
 
-import static org.uniproj.calculadora.Calculadora.calcularImc;
-import static org.uniproj.calculadora.Calculadora.statusImc;
+import static org.uniproj.calculadora.Calculadora.*;
 
 public class Main {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa(19, 61, 165, "Moxé", "manleto", "masculino");
         calcularImc(p1);
         String stats = statusImc(p1);
-        System.out.printf("%s\n%s", stats, p1.getImc());
+        System.out.printf("%s\n%s\n%s", stats, p1.getImc(), txGorduraCorporal(p1));
 
 
     }
