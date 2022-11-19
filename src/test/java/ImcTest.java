4093b10735f6dcdc3c068180b8a4f2339f97c2b7
Murilo Becker Adriano <AdrianoMurilo@edu.unisinos.br>
2022-11-18 21:50:58 -0300

    add test cases

--- src/test/java/ImcTest.java
+++ src/test/java/ImcTest.java
@@ -2,7 +2,16 @@ package org.uniproj;
 
 public class ImcTest {
 	
-	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(19, 75, 175, )
+	@Test
+	public void imcTest() {
+		
+		Pessoa p1 = new Pessoa(19, 75, 175, "schula", "alemão", "masculino");
+		Calaculadora c = new Calculadora();
+		
+		c.calcularImc(p1);
+		System.out.prntlin(p1.getImc());
+		c.statusImc(p1);
+		c.txGorduraCorporal(p1);
 	}
+	
 }
